3b899ac95e6cad042fcf175188f7faf8cbfc7263
Juliana Ferreira <julianafercavalcante@hotmail.com>
2023-09-26 21:10:21 -0300

    aluno inserido

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Usuario} from "../shared/model/usuario";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'social';
+  titulo : string = 'Social IFPB';
+  usuario : Usuario;
+  usuarios: Array<Usuario>;
+  aluno : string = 'Juliana Ferreira Cavalcante';
+
+  constructor() {
+    this.usuario = new Usuario();
+    this.usuarios = new Array<Usuario>;
+  }
+
+  inserirUsuario() : void {
+    this.usuarios.push(this.usuario);
+    this.usuario = new Usuario();
+  }
+
 }
